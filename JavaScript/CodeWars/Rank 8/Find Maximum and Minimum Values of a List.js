12b53425c5264673ee9c57fcf698a4c9c0fef0dd
freidertp <freidertp@gmail.com>
2023-03-06 16:40:04 -0500

    Update

--- JavaScript/CodeWars/Rank 8/Find Maximum and Minimum Values of a List.js
+++ JavaScript/CodeWars/Rank 8/Find Maximum and Minimum Values of a List.js
@@ -1,8 +1,6 @@
-let list = [1, 5, 3, 99, -8];
+/*Write a program in JS to find the maximum and minimum elements in an array.*/
 
 function maxAndMinNumber(list) {
   console.log(Math.min(...list));
   console.log(Math.max(...list));
 }
-
-maxAndMinNumber(list);
