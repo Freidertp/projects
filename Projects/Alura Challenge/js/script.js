f959d5cc0254ac764054e8f5dde601c95cda7f09
Freidertp <freidertp@gmail.com>
2023-05-01 13:54:04 -0500

    Update

--- Projects/Alura Challenge/js/script.js
+++ Projects/Alura Challenge/js/script.js
@@ -1,2 +1,12 @@
 document.querySelector("textarea").focus();
 document.querySelector("textarea").select();
+
+
+function lowerCase() {
+    let element = document.querySelector("textarea")
+    let value = element.value.toLowerCase()
+
+    element.value = value.replace(/[^a-z]/g, "")
+}
+
+
